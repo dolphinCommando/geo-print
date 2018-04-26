b14ceb85728a027ecd7882334d8fc5e4b1977047
Matthew Buchanan <matt@matthews-air.home>
2018-04-26 16:06:14 -0400

    uses HTML5 Geolocation and Google Maps Javascript API to show position

---
+++ assets/javascript/code.js
@@ -0,0 +1,35 @@
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 35.000, lng: -85.000},
+    zoom: 4
+  });
+  //infoWindow = new google.maps.InfoWindow;
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      //infoWindow.setPosition(pos);
+      //infoWindow.setContent('Location found.');
+      //infoWindow.open(map);
+      map.setCenter(pos);
+	  var marker = new google.maps.Marker({
+          position: pos,
+          map: map
+      });
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
