dd612d1f47640ea5c338d95ea03276516cf4efef
Matthew Buchanan <matt@matthews-air.home>
2018-04-27 21:40:18 -0400

    practice drawing shapes on Google Maps

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -20,6 +20,16 @@ function initMap() {
           position: pos,
           map: map
       });
+	  var circle = new google.maps.Circle({
+	    strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: pos,
+        radius: position.coords.accuracy
+	  });
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -27,6 +37,16 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+}
+function getCircle(accuracy) {
+  return {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'red',
+    fillOpacity: .2,
+    radius: accuracy,
+    strokeColor: 'white',
+    strokeWeight: .5
+  };
 }
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
